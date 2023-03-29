28d9dd90874cfe86e6c605cc08a57428a96471ff
Royalty <akannie33@gmail.com>
2023-03-29 16:45:27 +0100

    solution to task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - prints a binary tree in a post-order manner.
+ * @tree: pointer to the binary tree.
+ * @func: function pointer to a function for printing a node value.
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
