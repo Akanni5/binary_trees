b7214970d6f78a8def28773d33a26db05787a3ff
Royalty <akannie33@gmail.com>
2023-03-29 16:38:26 +0100

    solution to task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - prints a binary tree in a in-order manner.
+ * @tree: pointer to the binary tree.
+ * @func: function pointer to a function for printing a node value.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
