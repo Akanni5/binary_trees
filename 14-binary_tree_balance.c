21d97c48aaccefca7c27897861b0bf66973fbb99
Epicsociety <measkswho@gmail.com>
2023-03-30 20:52:05 +0300

    task 13-15

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_height - measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0 if tree is NULL or the height
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		int left = 1, right = 1;
+
+		if (tree->right)
+			right += binary_tree_height(tree->right);
+		if (tree->left)
+			left += binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	else
+		return (0);
+}
+
+/*
+ * binary_tree_balance - measures the balance factor of a binary tree 
+ * @tree:pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: 0 if tree is NULL or int (difference between 
+ * the heights of the left and right subtrees of that node)
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_height(tree->left) -
+			binary_tree_height(tree->right));
+}
